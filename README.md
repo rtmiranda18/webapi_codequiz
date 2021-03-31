64eef97151c97930aa6892b8a20b7a6a5bfa740f
Reesh Miranda <rtmiranda18@gmail.com>
2021-03-31 14:24:50 +0800

    add ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# webapi_codequiz
+# WEB API QUIZ
+
+## Site Picture
+![Site](codequiz.png)
+
+## Technologies Used
+- HTML - creates elements on the DOM
+- CSS - adds style to html elements
+- Javascript - adds special effects on pages
+- GitBash - for cloning repository and pushing code to GitHub
+- GitHub - holds repository that deploys to GitHub Pages
+
+## Summary
+This file contains the webpage for a Code Quiz. It also contains code that was created using HTML, CSS and Javascript. 
+
+## Code Snippet
+```html
+function countdown(timeLeft) {
+    var quizTimer = setInterval(function(){
+        if(timeLeft <= 0){
+            clearInterval(quizTimer);
+        document.getElementById("countdown").innerHTML = "Finished";
+        } else {
+        document.getElementById("countdown").innerHTML = timeLeft + " seconds remaining";
+    }
+    timeLeft -= 1;
+    }, 1000);
+}
+```
+
+```html
+<html>
+
+</html>
+```
+
+## Author Links 
+[LinkedIn](https://www.linkedin.com/in/rosario-miranda-b81170132/)
+[GitHub](https://github.com/rtmiranda18)
--- js/script.js
+++ js/script.js
@@ -54,10 +54,10 @@ function showQuestions(index){
 
     //creating a new span and div tag for question and option and passing the value using array index
     let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
-    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
-                    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
-                    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
-                    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    let option_tag = '<div class="option "><span>'+ questions[index].options[0] +'</span></div>'
+                    + '<div class="option "><span>'+ questions[index].options[1] +'</span></div>'
+                    + '<div class="option "><span>'+ questions[index].options[2] +'</span></div>'
+                    + '<div class="option "><span>'+ questions[index].options[3] +'</span></div>';
     //adding new span tag inside que_tag
     que_text.innerHTML = que_tag; 
     //adding new div tag inside option_tag
